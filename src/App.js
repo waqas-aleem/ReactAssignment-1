7d5589ac1c5ab8a6450d4dcdb4625026479d23f4
waqas-aleem <waqas.aleem1990@gmail.com>
2022-01-11 17:54:19 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -156,53 +156,53 @@ function App() {
 </div>
 {/* best burger */}
 {/* <!-- Big Burger --> */}
-<div  class=" container-fluid bigburderarea">
-  <div class="row">
-    <div class="col-md-2 col-sm-12 col-lg-4">
+<div  className=" container-fluid bigburderarea">
+  <div className="row">
+    <div className="col-md-2 col-sm-12 col-lg-4">
    
-       <img src={Shape4} alt="" class="ourchefshape4 mt-5 ml-0" style={{transform: "scaleX(-1)"}}/>
+       <img src={Shape4} alt="" className="ourchefshape4 mt-5 ml-0" style={{transform: "scaleX(-1)"}}/>
    
     </div>
     <div class="col-md-8 col-sm-12 col-lg-4 text-center"  >
        
-      <img src={Shape2} alt="" class="ourchefshape2 " style={{transform: "scaleX(-1)", marginTop:"0px"}}/>
+      <img src={Shape2} alt="" className="ourchefshape2 " style={{transform: "scaleX(-1)", marginTop:"0px"}}/>
        
       </div>
-      <div class="col-md-2 col-sm-12 col-lg-4 text-end"  >
+      <div className="col-md-2 col-sm-12 col-lg-4 text-end"  >
        
-          <img src={Shape5} alt="" class="ourchefshape4 mt-5 ml-0"/>
+          <img src={Shape5} alt="" className="ourchefshape4 mt-5 ml-0"/>
         
         </div>
   </div>
 
-  <div class="row">
-    <div class="col-md-6 ">
-      <div class="mt-5 text-center"  >
-      <p class="bigburderheading">Big Burger</p>
-      <p class="bigburgerp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p>
-      <button class="orderbtn"> Order Now </button>
+  <div className="row">
+    <div className="col-md-6 ">
+      <div className="mt-5 text-center"  >
+      <p className="bigburderheading">Big Burger</p>
+      <p className="bigburgerp">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. </p>
+      <button className="orderbtn"> Order Now </button>
   </div>
     </div>
-    <div class="col-md-6">
+    <div className="col-md-6">
     
-      <div class="mt-5 text-center" >
-        <img src={burger} alt="" class="bestburgerimg"/>
+      <div className="mt-5 text-center" >
+        <img src={burger} alt="" className="bestburgerimg"/>
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-md-4 col-lg-4 text-center">
-      <img src={Shape1} class="ourchefshape1 mb-5" alt="" style={{transform: "scaleX(-1)"}}/>
+  <div className="row">
+    <div className="col-md-4 col-lg-4 text-center">
+      <img src={Shape1} className="ourchefshape1 mb-5" alt="" style={{transform: "scaleX(-1)"}}/>
     </div>
 
 
-    <div class="col-md-4 col-lg-4 text-center">
-      <img src={Shape6} alt="" class="ourchefshape6" style={{transform: "rotate(180deg)"}}/>
+    <div className="col-md-4 col-lg-4 text-center">
+      <img src={Shape6} alt="" className="ourchefshape6" style={{transform: "rotate(180deg)"}}/>
       
     </div>
    
-    <div class="col-md-4 col-lg-4 text-end">
-    <img src="assets/Shape3.png" alt="" class="ourchefshape3" style={{transform: "scaleX(-1)"}}/>
+    <div className="col-md-4 col-lg-4 text-end">
+    <img src={Shape3}   alt="" className="ourchefshape3" style={{transform: "scaleX(-1)"}}/>
   </div>
   </div>
 </div>
